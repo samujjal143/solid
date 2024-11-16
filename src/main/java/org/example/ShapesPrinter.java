7808d8324edaf32a5904b48cfea3bd463601ded1
Samujjal Das <samujjal.das@piramal.com>
2024-11-16 21:57:00 +0530

    Dependency Inversion Principle

--- src/main/java/org/example/ShapesPrinter.java
+++ src/main/java/org/example/ShapesPrinter.java
@@ -1,12 +1,19 @@
 package org.example;
 
+import java.util.List;
+
 public class ShapesPrinter {
+    private AreaCalculator areaCalculator = new AreaCalculator();
 
-    public String json(int sum) {
-        return "{shapesSum: %s}".formatted(sum);
+    public String json(List<Shape> shapes) {
+        return "{shapesSum: %s}".formatted(areaCalculator.sum(shapes));
     }
 
-    public String csv(int sum) {
-        return "shapes_sum, %s".formatted(sum);
+    public String csv(List<Shape> shapes) {
+        return "shapes_sum, %s".formatted(areaCalculator.sum(shapes));
     }
+
+    /*  We are breaking the Dependency Inversion Principle because we are depending on the
+        actual concrete class i.e. AreaCalculator
+     */
 }
